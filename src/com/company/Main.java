e154d1d0ba961c3faba7d71cffb6541cf00175c9
Oleg <o.borman29@gmail.com>
2021-11-02 11:41:39 +0100

    zmena v main posledni radek

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
         System.out.println("Staty s limitem nad: " + limit + ":");
         for (Stat s : vypisStaty) {
-            System.out.println(s.getNazevStatu() + " (" + s.getZakladniSazba() + " %)");
+            System.out.println(s.vypis());
         }
     }
 }
