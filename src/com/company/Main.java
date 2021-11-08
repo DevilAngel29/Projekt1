e8c20360ca52f155fda7532ad00b0ee828c368b3
Oleg <o.borman29@gmail.com>
2021-11-08 12:22:32 +0100

    doplneni zbytku zadani

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,39 @@
 package com.company;
 
-import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static final String FILENAME = "vat-eu,csv.txt";
 
-
     public static void main(String[] args) {
         Model model = new Model();
-
         try {
             model.importFromTextFile(FILENAME);
         } catch (ModelException e) {
             System.err.println("Nepodarilo se nacist data ze souboru. " + e.getMessage());
         }
 
-
-
-       System.out.println("Vlozte limit sazby DPH:");
+        try {
+            model.exportToFile(FILENAME);
+        } catch (ModelException e) {
+            System.err.println("Nepodarilo se nacist data do souboru. " + e.getMessage());
+        }
+        System.out.println("Vlozte limit sazby DPH:");
         Scanner input = new Scanner(System.in);
         double limit = input.nextDouble();
+        System.out.println();
         ArrayList<Stat> vypisStaty = model.splnujeKriteria(limit);
-
-
-        System.out.println("Staty s limitem nad: " + limit + "%:");
+        System.out.println("Staty s limitem nad " + limit + "% a nepouziva specialni sazbu z dane:");
         for (Stat s: vypisStaty) {
             System.out.println(s.nazevStatu +"("+ s.getZakladniSazba()+"%)");
+        }
+
+        ArrayList<Stat> nesplnuje = model.nesplnujeKriteria(limit);
+        System.out.println("Staty s limitem pod " + limit + "% nebo pouzivaji specialni sazbu z dane:");
+        for (Stat s: nesplnuje) {
+            System.out.println(s.zkratkaStatu);
         }
 
     }
--- src/com/company/Model.java
+++ src/com/company/Model.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Model {
-     ArrayList<Stat> statDph = new ArrayList<>();
+    public static final String TAB = "\t";
+    ArrayList<Stat> statDph = new ArrayList<>();
 
     public Model importFromTextFile(String fileName) throws ModelException{
         Model model = new Model();
+        System.out.println("Vypis vsech statu:");
         try (Scanner scanner = new Scanner(new FileInputStream(fileName))) {
             while(scanner.hasNextLine()) {
                 String inputLine = scanner.nextLine();
-                String[] polozky = inputLine.split("\t");
+                String[] polozky = inputLine.split(TAB);
                 if (polozky.length != 5)
                     throw new ModelException("Nespravny pocet polozek na radku:" + inputLine + polozky.length);
                 Stat stat = new Stat(polozky[4], polozky[3], polozky[2], polozky[1], polozky[0]);
                 statDph.add(stat);
                 System.out.println(stat.vypis());
-
-
             }
         } catch (FileNotFoundException ex) {
             throw new ModelException("Soubor"+ fileName+" nenalezen:"+ex.getLocalizedMessage());
@@ -27,11 +27,37 @@ public class Model {
         return model;
     }
 
+    public void exportToFile(String filename) throws ModelException {
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(filename))) {
+            for (Stat stat : statDph) {
+                writer.println(
+                        stat.getZkratkaStatu()+ TAB
+                        +stat.getNazevStatu()+ TAB
+                        +stat.getZakladniSazba()+ TAB
+                        +stat.getSnizenaSazba()+ TAB
+                        +stat.parkingRate);
+            }
+        } catch (FileNotFoundException e) {
+            throw new ModelException("Soubor "+filename+" nenalezen: "+e.getLocalizedMessage());
+        }
+    }
 
     public ArrayList<Stat> splnujeKriteria(double limit) {
         ArrayList<Stat> result = new ArrayList<>();
+
         for (Stat stat : statDph) {
-            if ( stat.zakladniSazba > limit) {
+            if ( stat.zakladniSazba > limit && stat.parkingRate == false) {
+                result.add(stat);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Stat> nesplnujeKriteria(double limit) {
+        ArrayList<Stat> result = new ArrayList<>();
+
+        for (Stat stat : statDph) {
+            if ( stat.zakladniSazba < limit || stat.parkingRate == true) {
                 result.add(stat);
             }
         }
